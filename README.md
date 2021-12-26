cb6c326ddc05e54b3f5bbc16001dbe2c96fa7804
stho32 <stho32github@gmail.com>
2021-12-26 15:55:00 +0100

    [A003] Unit Tests für den Spamfilter aufgrund invalider E-Mail-Adressen hinzugefügt

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ Eine Challenge aus https://github.com/stho32/Collection-Of-Challenges
 - [X] (A001) Die Anwendung verbindet sich per IMAP mit deinem E-Mail Account
 - [ ] (A002) Die Anwendung implementiert mindestens 3 Regeln für die Blacklist-Identifikation von Spam, z.B.
   - [X] (A003) E-Mails, die keinen Absender oder keine oder keine valide E-Mail-Adresse (mindestens *@*.*) im Absender haben, sind Spam
-  - [ ] (A004) E-Mails, die "Amazon" in dem Namen des Absenders angeben, aber nicht von der Domain amazon.de oder amazon.com gesendet wurden, sind Spam
+  - [X] (A004) E-Mails, die "Amazon" in dem Namen des Absenders angeben, aber nicht von der Domain amazon.de oder amazon.com gesendet wurden, sind Spam
   - [ ] (A005) E-Mails, die in Betreff oder Inhalt bestimmte Schlüsselwörter beinhalten sind Spam
 - [ ] (A007) Es gibt mindestens 1 Regel für White-Listing, d.h. expliziten Spam-Ausschluss
   - [ ] (A008) Es gibt die Möglichkeit ein Adressbuch von Kontakten zu konfigurieren, die auf jeden Fall nicht als Spam angesehen werden
---
+++ Source/spamfilter/spamfilter.BL.Tests/SpamDetectors/InvalidSenderEmailAddressSpamDetectorTests.cs
@@ -0,0 +1,43 @@
+using spamfilter.BL.Entities;
+using spamfilter.BL.Helpers;
+using spamfilter.BL.SpamDetectors;
+using spamfilter.Interfaces;
+using Xunit;
+
+namespace spamfilter.BL.Tests.SpamDetectors;
+
+public class InvalidSenderEmailAddressSpamDetectorTests
+{
+    private ISpamDetector SpamDetector()
+    {
+        var detector = new InvalidSenderEmailAddressSpamDetector(
+            new EmailValidator());
+
+        return detector;
+    }
+    
+    [Fact]
+    public void When_the_attached_emailvalidator_is_satisfied_we_get_OK()
+    {
+        var mail = new Email(
+            "Amazon",
+            "amazon@amazon.de",
+            "123",
+            "Your orders");
+        
+        Assert.Empty(SpamDetector().GetOpinionsOn(mail));
+    }
+    
+    [Fact]
+    public void When_the_attached_emailvalidator_is_not_satisfied_it_is_spam()
+    {
+        var mail = new Email(
+            "Amazon",
+            "Not-an-email",
+            "123",
+            "Your orders");
+        
+        Assert.NotEmpty(SpamDetector().GetOpinionsOn(mail));
+    }
+
+}
