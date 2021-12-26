8dc59d737db7d64392702e1bf1fdafa6a496c767
stho32 <stho32github@gmail.com>
2021-12-26 03:25:47 +0100

    Experimental usage of mailkit library

--- Source/spamfilter/spamfilter.console/Program.cs
+++ Source/spamfilter/spamfilter.console/Program.cs
@@ -1,3 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using MailKit.Net.Imap;
+using MailKit;
 
-Console.WriteLine("Hello, World!");
+using (var client = new ImapClient ()) {
+    Console.Write("Server: ");
+    var server = Console.ReadLine();
+    client.Connect (server, 993, true);
+
+    Console.Write("Username: ");
+    var username = Console.ReadLine();
+    Console.Write("Password: ");
+    var password = Console.ReadLine();
+    
+    client.Authenticate (username, password);
+
+    // The Inbox folder is always available on all IMAP servers...
+    var inbox = client.Inbox;
+    inbox.Open (FolderAccess.ReadOnly);
+
+    Console.WriteLine ("Total messages: {0}", inbox.Count);
+    Console.WriteLine ("Recent messages: {0}", inbox.Recent);
+
+    for (int i = 0; i < inbox.Count; i++) {
+        var message = inbox.GetMessage (i);
+        Console.WriteLine ("Subject: {0}", message.Subject);
+    }
+
+    client.Disconnect (true);
+}
