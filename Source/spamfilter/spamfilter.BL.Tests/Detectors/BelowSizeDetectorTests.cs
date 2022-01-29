17c8c634e6fdca5a0042ab42ab06f8a21723bd34
stho32 <stho32github@gmail.com>
2022-01-29 18:47:21 +0100

    Verified, that the BelowSizeDetector detects correctly

--- Source/spamfilter/spamfilter.BL.Tests/Detectors/BelowSizeDetectorTests.cs
+++ Source/spamfilter/spamfilter.BL.Tests/Detectors/BelowSizeDetectorTests.cs
@@ -29,13 +29,20 @@ public class BelowSizeDetectorTests
         Assert.NotEmpty(detector.GetOpinionsOn(email));
     }
     
-    /*
-     *
-     * [Fact]
-    public void When_an_email_is_below_a_specified_size_it_is_detected()
+    [Fact]
+    public void When_an_email_is_bigger_than_a_specified_size_it_is_not_detected()
     {
-        
+        var email = new Email(
+            "",
+            "",
+            UniqueId.Invalid,
+            "This is a subject",
+            "This body is bigger than 20 bytes."
+        );
+
+        var detector = BelowSizeDetector(20);
+
+        Assert.Empty(detector.GetOpinionsOn(email));
     }
-     */
     
 }
