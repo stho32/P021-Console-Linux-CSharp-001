af6832520b2756151d4e105fa3f3f9fb40a0ada1
stho32 <stho32github@gmail.com>
2022-01-30 18:39:33 +0100

    Add test coverage for domain expression based detector

---
+++ Source/spamfilter/spamfilter.BL.Tests/Detectors/DomainExpressionBasedDetectorTests.cs
@@ -0,0 +1,64 @@
+using MailKit;
+using spamfilter.BL.Detectors;
+using spamfilter.BL.Entities;
+using spamfilter.Interfaces;
+using Xunit;
+
+namespace spamfilter.BL.Tests.Detectors;
+
+public class DomainExpressionBasedDetectorTests
+{
+    private IDetector DetectorUnderTest(string regEx)
+    {
+        return new DomainExpressionBasedDetector(regEx);
+    }
+
+    [Fact]
+    public void When_the_domain_is_matching_the_regular_expression_it_detects()
+    {
+        var email = new Email(
+            "",
+            "brian@amazon.de",
+            UniqueId.Invalid,
+            "",
+            ""
+        );
+
+        var detector = DetectorUnderTest(@".?\.de");
+        
+        Assert.NotEmpty(detector.GetOpinionsOn(email));
+    }
+
+    [Fact]
+    public void When_the_domain_is_not_matching_the_regular_expression_it_does_not_detect()
+    {
+        var email = new Email(
+            "",
+            "brian@amazon.de",
+            UniqueId.Invalid,
+            "",
+            ""
+        );
+
+        var detector = DetectorUnderTest(@".?\.com");
+        
+        Assert.Empty(detector.GetOpinionsOn(email));        
+    }
+
+    [Fact]
+    public void
+        When_the_regular_expression_would_match_the_part_before_the_domain_but_not_with_the_domain_it_does_not_detect()
+    {
+        var email = new Email(
+            "",
+            "brian@amazon.de",
+            UniqueId.Invalid,
+            "",
+            ""
+        );
+
+        var detector = DetectorUnderTest(@"b.?n");
+        
+        Assert.Empty(detector.GetOpinionsOn(email));        
+    }
+}
